ed3c5fb88f6ce3b1ab464f82b9adc7ffd4d2e618
KristofferDN <cakepaul@hotmail.com>
2017-12-17 01:23:24 +0100

    Added instance of TeleportCommand to handler.

--- FireFlake/FireFlakeCore/src/no/fireflake/core/Core.java
+++ FireFlake/FireFlakeCore/src/no/fireflake/core/Core.java
@@ -2,6 +2,7 @@ package no.fireflake.core;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import no.fireflake.core.commands.TeleportCommand;
 import no.fireflake.core.commands.TestCommand;
 
 public class Core extends JavaPlugin {
@@ -15,6 +16,7 @@ public class Core extends JavaPlugin {
 		getServer().getPluginManager().registerEvents(new Listener(), this);
 		
 		handler.registerCommand(new TestCommand());
+		handler.registerCommand(new TeleportCommand());
 	}
 	
 }
